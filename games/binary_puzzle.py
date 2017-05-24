9d7bd4227e09b7496c669c3fea08483d6dbaa57c
nihlaeth <info@nihlaeth.nl>
2017-05-24 17:47:26 +0200

    refactor binary puzzle solution generation Previous solution took about 24 seconds to generate every six by six solution. This one takes about 36 seconds to do the same, but it offers more flexibility in optimising.
    The idea is to only visit squares built from valid rows. This greatly reduces
    the amount of squares visited, at the cost of memory (we have to keep every possible
    valid row in memory). The problem is that my implementation isn't very efficient.
    Especially the list comprehensions seem to be taking a lot of time, so conversion
    to a one-dimensional list so that we don't need to generate separate column lists
    seems proper here. After that it might be worth considering
    writing part of the code in C.
    
    This implementation does not work well for larger puzzle sizes because of the memory footprint.
    since this is binary, we might try using actual bits instead of multiple bytes per number.
    
    We could also break the rows up into multiple blocks to be combined one at a time.

--- games/binary_puzzle.py
+++ games/binary_puzzle.py
@@ -1,15 +1,5 @@
 """Tools for generating binary puzzles."""
-from itertools import tee, islice
-from functools import partial
-from constraint import (
-    Problem,
-    ExactSumConstraint,
-    MaxSumConstraint,
-    MinSumConstraint,
-    FunctionConstraint,
-    BacktrackingSolver,
-    RecursiveBacktrackingSolver,
-    MinConflictsSolver)
+from itertools import tee, islice, permutations, product
 
 def _nwise(iterable, n=2):
     iters = tee(iterable, n)
@@ -17,70 +7,53 @@ def _nwise(iterable, n=2):
         next(islice(it, i, i), None)
     return zip(*iters)
 
-def _test_uniqueness(*values, dimension=6):
-    rows = set()
-    columns = set()
-    for i in range(dimension):
-        row_positions = range(
-            i * dimension,
-            (i + 1) * dimension)
-        column_positions = range(i, dimension**2, dimension)
-        rows.add("".join([
-            str(values[position]) for position in row_positions]))
-        columns.add("".join([
-            str(values[position]) for position in column_positions]))
-    if len(rows) < dimension or len(columns) < dimension:
+def _test_array_validity(values):
+    # same number of zeros and ones in each column
+    if values.count(0) != len(values) / 2:
+        return False
+    # maximum two of the same next to each other
+    for triplet in _nwise(values, 3):
+        total = sum(triplet)
+        if total < 1 or total > 2:
+            return False
+    return True
+
+def _test_columns_validity(data):
+    columns = []
+    for i in range(len(data)):
+        column = tuple(row[i] for row in data)
+        if not _test_array_validity(column):
+            return False
+        columns.append(column)
+    # test column uniqueness
+    if len(set(columns)) != len(data):
         return False
     return True
 
-def solution(dimension, solver=None):
-    """Generate binary puzzle solution."""
-    problem = Problem()
-    if solver is not None:
-        problem.setSolver(solver)
-    else:
-        problem.setSolver(BacktrackingSolver())
-    problem.addVariables(range(dimension**2), [0, 1])
-    for i in range(dimension):
-        row_positions = range(
-            i * dimension,
-            (i + 1) * dimension)
-        column_positions = range(i, dimension**2, dimension)
-        # same number of zeros and ones in each row
-        problem.addConstraint(
-            ExactSumConstraint(dimension / 2), row_positions)
-        problem.addConstraint(
-            ExactSumConstraint(dimension / 2), column_positions)
-        # maximum two of the same next to each other
-        for triplet in _nwise(row_positions, 3):
-            problem.addConstraint(
-                MaxSumConstraint(2), triplet)
-            problem.addConstraint(
-                MinSumConstraint(1), triplet)
-        for triplet in _nwise(column_positions, 3):
-            problem.addConstraint(
-                MaxSumConstraint(2), triplet)
-            problem.addConstraint(
-                MinSumConstraint(1), triplet)
-    # unique rows and columns
-    problem.addConstraint(
-        FunctionConstraint(
-            partial(_test_uniqueness, dimension=dimension)),
-        range(dimension**2))
-    if isinstance(solver, RecursiveBacktrackingSolver):
-        return problem.getSolutions()
-    if isinstance(solver, MinConflictsSolver):
-        return (problem.getSolution(),)
-    return problem.getSolutionIter()
+def _get_possible_rows(dimension):
+    row_content = [0] * (dimension // 2) + [1] * (dimension // 2)
+    possible_rows = set(permutations(row_content, dimension))
+    return tuple(row for row in possible_rows if _test_array_validity(row))
+
+def solutions(dimension):
+    """Generate binary puzzle solutions."""
+    assert dimension % 2 == 0
+
+    possible_rows = _get_possible_rows(dimension)
+    for indices in permutations(range(len(possible_rows)), dimension):
+        possible_solution = tuple(possible_rows[i] for i in indices)
+        if _test_columns_validity(possible_solution):
+            yield possible_solution
 
 def start():
     """Handle command line interface."""
-    dimension = 18  # hardcode this for now
-    result = solution(dimension)
+    dimension = 6  # hardcode this for now
+    result = solutions(dimension)
     for item in result:
-        for i in range(dimension):
-            row_positions = range(
-                i * dimension,
-                (i + 1) * dimension)
-            print(" ".join([str(item[position]) for position in row_positions]))
+        for row in item:
+            print(" ".join([str(cell) for cell in row]))
         print()
+
+if __name__ == "__main__":
+    import cProfile
+    cProfile.run('start()')
---
+++ tests/test_binary_puzzle.py
@@ -0,0 +1,49 @@
+"""Tests for binary puzzle tools."""
+from time import perf_counter
+import pytest
+from games.binary_puzzle import (
+    solutions, _test_array_validity, _test_columns_validity)
+
+def test_array_validity():
+    assert _test_array_validity([0, 0, 1, 1])
+    assert _test_array_validity([0, 1, 0, 1])
+    assert not _test_array_validity([0, 1, 1, 1])
+    assert not _test_array_validity([1, 0, 0, 0, 1, 1])
+
+def test_columns_validity():
+    # valid solution
+    assert _test_columns_validity(((0, 1), (1, 0)))
+    assert _test_columns_validity((
+        (1, 0, 0, 1),
+        (0, 1, 1, 0),
+        (0, 1, 0, 1),
+        (1, 0, 1, 0)))
+    # invalid columns
+    assert not _test_columns_validity((
+        (1, 0, 0, 1),
+        (0, 1, 1, 0),
+        (0, 1, 0, 1),
+        (1, 0, 0, 1)))
+    # duplicate columns
+    assert not _test_columns_validity((
+        (1, 0, 0, 1),
+        (0, 1, 1, 0),
+        (0, 1, 1, 0),
+        (1, 0, 0, 1)))
+
+def _delta_time(function):
+    start = perf_counter()
+    function()
+    end = perf_counter()
+    return end - start
+
+def _performance():
+    dimension_six = _delta_time(
+        lambda: list(solutions(dimension=6)))
+    print(f"dimension=6, all solutions: {dimension_six}")
+    # dimension_thirty_six = _delta_time(
+    #     lambda: next(solution(dimension=16)))
+    # print(f"dimension=36, one solution: {dimension_thirty_six}")
+
+if __name__ == "__main__":
+    _performance()
